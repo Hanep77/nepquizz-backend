f3960e70f70bc6d6ad221ab13391a1d67aad4564
hanep <yudishan26@gmail.com>
2024-06-14 20:13:32 +0700

    create method that return all data

--- app/Http/Controllers/DifficulityController.php
+++ app/Http/Controllers/DifficulityController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Difficulity;
 use Illuminate\Http\Request;
 
 class DifficulityController extends Controller
 {
-    //
+    public function index()
+    {
+        return Difficulity::query()->get();
+    }
 }
--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\QuizResource;
+use App\Models\Quiz;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
-    //
+    public function index()
+    {
+        $quizzes = Quiz::query()->with(["user", "category", "difficulity"])->get();
+        return QuizResource::collection($quizzes);
+    }
 }
